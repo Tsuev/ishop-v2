da525517bec0049fc21377a32ccac289cad163ff
Tsuev <tsuevjudoka@gmail.com>
2024-03-12 13:49:38 +0300

    Add modules

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex justify-center pt-10">
+    <h1 class="text-2xl">Привет это IShop V2</h1>
   </div>
 </template>
+
+<script lang="ts"></script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@pinia/nuxt", "nuxt-primevue"],
+  css: [
+    "@/assets/styles/tailwind.scss",
+    "primevue/resources/themes/aura-dark-blue/theme.css",
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
